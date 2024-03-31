3746c9a8b9c797cd381c38c383208b42dc712454
Viraviu7 <vvillarreal@unimagdalena.edu.co>
2024-03-31 15:54:00 -0500

    add ProductoRepository

--- src/main/java/com/shop/eShop/repositories/ProductoRepository.java
+++ src/main/java/com/shop/eShop/repositories/ProductoRepository.java
@@ -1,14 +1,19 @@
 package com.shop.eShop.repositories;
 
-import java.util.Optional;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 import com.shop.eShop.entities.Producto;
 
 @RepositoryRestController
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    /* find by id */
-    Optional<Producto> findById(Long id);
+
+    /* find by nombre */
+    @Query("SELECT p FROM Producto p WHERE LOWER(p.nombre) LIKE LOWER(CONCAT('%', ?1, '%'))")
+    List<Producto> findByNombre(String nombre);
+
 }
